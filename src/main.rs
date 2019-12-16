96dd10fd39c4050a6b17c11f72e9d4faa6880ab8
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2019-12-16 20:18:51 +0800

    Cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ extern crate rocket;
 extern crate reqwest;
 use std::sync::{Arc, Mutex, RwLock};
 
-const NEBULA_ADDR : &str = "nebula-addr";
-const NEBULA_PORT : &str = "nebula-port";
-const PORT : &str = "port";
+const NEBULA_ADDR: &str = "nebula-addr";
+const NEBULA_PORT: &str = "nebula-port";
+const PORT: &str = "port";
 
 // TODO(shylock) Cache the metrics?
 // NOT Cache metrics now
@@ -57,29 +57,32 @@ fn main() {
                 .long(NEBULA_ADDR)
                 .help("Specify the nebula metric expose address")
                 .default_value("localhost")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             clap::Arg::with_name(NEBULA_PORT)
                 .long(NEBULA_PORT)
                 .help("Specify the nebula metric expose port")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             clap::Arg::with_name(PORT)
                 .long(PORT)
                 .help("Specify the prot to expose metrics data encoded in prometheus format")
                 .default_value("2333")
-                .takes_value(true)
+                .takes_value(true),
         )
         .get_matches();
     let nebula_addr = matches.value_of(NEBULA_ADDR).unwrap();
     let nebula_port = matches.value_of(NEBULA_PORT).unwrap();
-    info!("Scrape raw metrics from http://{}:{}/metrics!", nebula_addr, nebula_port);
+    info!(
+        "Scrape raw metrics from http://{}:{}/metrics!",
+        nebula_addr, nebula_port
+    );
     let port = matches.value_of(PORT).unwrap().parse::<u16>().unwrap();
     info!("Expose at port http://localhost:{}/metrics!", port);
 
-    let url : String = format!("http://{}:{}/metrics", nebula_addr, nebula_port);
+    let url: String = format!("http://{}:{}/metrics", nebula_addr, nebula_port);
     {
         URL.lock().unwrap().push_str(&url);
         warn!("The url: {}", URL.lock().unwrap());
@@ -96,16 +99,14 @@ fn main() {
         .launch();
 }
 
-#[derive(Deserialize, Serialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Gauge {
     pub name: String,
     pub value: i64,
     pub labels: Vec<String>,
 }
 
-#[derive(Deserialize, Serialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Histogram {
     pub name: String,
     pub value_range: [i64; 2],
@@ -113,8 +114,7 @@ struct Histogram {
     pub lables: Vec<String>,
 }
 
-#[derive(Deserialize, Serialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Metrics {
     gauges: Vec<Gauge>,
     histograms: Vec<Histogram>,
@@ -122,11 +122,11 @@ struct Metrics {
 
 impl Metrics {
     fn gauges(&self) -> &Vec<Gauge> {
-        return &self.gauges
+        return &self.gauges;
     }
 
     fn histograms(&self) -> &Vec<Histogram> {
-        return &self.histograms
+        return &self.histograms;
     }
 }
 
@@ -134,8 +134,8 @@ fn setup_logging() {
     use log::LevelFilter;
     use log4rs::append::console::ConsoleAppender;
     use log4rs::append::file::FileAppender;
-    use log4rs::encode::pattern::PatternEncoder;
     use log4rs::config::{Appender, Config, Logger, Root};
+    use log4rs::encode::pattern::PatternEncoder;
 
     let stdout = ConsoleAppender::builder().build();
 
@@ -148,10 +148,12 @@ fn setup_logging() {
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
         .appender(Appender::builder().build("filelog", Box::new(filelog)))
         .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
-        .logger(Logger::builder()
-            .appender("filelog")
-            .additive(false)
-            .build("app::requests", LevelFilter::Info))
+        .logger(
+            Logger::builder()
+                .appender("filelog")
+                .additive(false)
+                .build("app::requests", LevelFilter::Info),
+        )
         .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
         .unwrap();
 
@@ -166,12 +168,10 @@ fn get_metrics() -> String {
     return prometheus_format(&metrics);
 }
 
-
 #[get("/")]
 fn hello() -> &'static str {
     "The Prometheus metrics exposer for Nebula Graph! Get the metrics from /metrics.\n"
 }
-
 
 // Format Prometheus
 fn prometheus_format(m: &Metrics) -> String {
@@ -179,22 +179,28 @@ fn prometheus_format(m: &Metrics) -> String {
     let encoder = prometheus::TextEncoder::new();
     // Gauges
     for g in m.gauges() {
-        let gauge_option = prometheus::Opts::new(g.name.clone(), "Record all gauges about nebula".to_string());
+        let gauge_option =
+            prometheus::Opts::new(g.name.clone(), "Record all gauges about nebula".to_string());
         let gauge = prometheus::Gauge::with_opts(gauge_option).unwrap();
         gauge.set(g.value as f64);
         reg.register(Box::new(gauge.clone())).unwrap();
     }
     // Now we expose the gauge computed from histogram!
     // We need to construct histogram with buckets filled!
-    //for h in m.histograms() {
-        //let histogram_option = prometheus::HistogramOpts::new(h.name.clone(), "Record all histograms about Nebula".to_string());
-        //let mut buckets: Vec<f64> = vec![];
-        ////for b in &h.buckets {  It's bound
-            ////buckets.push(*b as f64);
-        ////}
-        //let histogram = prometheus::Histogram::with_opts(histogram_option.buckets(buckets)).unwrap();
-        //reg.register(Box::new(histogram.clone())).unwrap();
-    //}
+    // for h in m.histograms() {
+    // let histogram_option = prometheus::HistogramOpts::new(
+    // h.name.clone(),
+    // "Record all histograms about Nebula".to_string(),
+    // );
+    // let mut buckets: Vec<f64> = vec![];
+    // for b in &h.buckets {
+    // // It's bound
+    // buckets.push(*b as f64);
+    // }
+    // let histogram =
+    // prometheus::Histogram::with_opts(histogram_option.buckets(buckets)).unwrap();
+    // reg.register(Box::new(histogram.clone())).unwrap();
+    // }
     let mut buffer = vec![];
     let metrics = reg.gather();
     encoder.encode(&metrics, &mut buffer).unwrap();
